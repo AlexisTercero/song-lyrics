130f0c2e2dd27520e0da14d1a0435c355f44fc94
Alexis Tercero <alexisdealmeyda@gmail.com>
2021-06-02 20:52:29 -0300

    ADD Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,9 +2,52 @@ import React from 'react'
 
 const Formulario = () => {
     return (
-        <h1>
-         Form
-        </h1>
+        <div className="b">
+            <div className="container">
+                <div className="row">                    
+                    <form 
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    >
+                        <fieldset>
+                            <legend className="text-center">mmmLyRICS!</legend>
+
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>ARTIST</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="artista"
+                                            placeholder="Nombre Artista"
+                                        />
+                                    </div>
+                                    
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>SONG</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="cancion"
+                                            placeholder="Nombre Canción"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="divbut col text-center">
+                            <button 
+                                type="submit" 
+                                className="btn "
+                            >Sing!</button>
+                            </div>
+                        </fieldset>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
